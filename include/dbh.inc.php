15d25dcd8ee2543cfb204b2d49816b65e4f8eb34
rindo789 <rindo.ondrejka@gmail.com>
2021-09-21 22:33:27 +0200

    Create dbh.inc.php

---
+++ include/dbh.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+function OpenCon()
+{
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $db = "projekt2";
+    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Napojenie zlyhalo: %s\n". $conn -> error);
+    return $conn;
+}
+
+function CloseCon($conn)
+{
+ $conn -> close();
+}
+
+
+?>
