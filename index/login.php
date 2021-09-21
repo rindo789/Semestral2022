e0d31e7371be111ed38fc8dac75afb79d1c86f57
rindo789 <rindo.ondrejka@gmail.com>
2021-09-21 22:33:37 +0200

    Create login.php

---
+++ index/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form action="../include/login.inc.php" method="POST">
+        <p>Meno</p>
+        <input type="text" placeholder="Nick" name="userId">
+        <p>Heslo</p>
+        <input type="text" placeholder="Password" name="passWrd">
+        <input type="submit" name="submit" value="Log In">
+    </form>
+    <?php include "../include/functions.inc.php";
+        errorEcho();
+    ?>
+</body>
+<footer>
+
+</footer></html>
